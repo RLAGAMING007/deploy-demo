264c54251861abc8f5e3fa97af7fec961be671e5
RLAGAMING007 <k.l.kumar006@gmail.com>
2024-02-18 12:57:36 +0530

    feat: react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import { Content } from "./pages/Content.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/deploy-demo/",
+    element: <App />,
+    children: [
+      {
+        path: "/deploy-demo/",
+        element: <Home />,
+      },
+      {
+        path: "/deploy-demo/content",
+        element: <Content />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
